7576d05a10339d59512a21bf93654e1d90a65272
vishnureghu1 <vishnureghu21@gmail.com>
2025-05-03 21:08:56 +0530

    all

--- Front-End/src/App.vue
+++ Front-End/src/App.vue
@@ -1,8 +1,19 @@
+<script setup>
+import Header from "@/components/common/Header.vue";
+import Footer from "@/components/common/Footer.vue";
+import Sidebar from "@/components/common/Sidebar.vue";
+import { ref } from "vue";
+import { onMounted } from "vue";
+
+const drawer = ref(true);
+const sidebarRef = ref(null);
+</script>
+
 <template>
   <v-app>
     <Header />
-    <Sidebar />
-    <v-main>
+    <Sidebar ref="sidebarRef" />
+    <v-main :class="{ 'with-sidebar-expanded': sidebarRef?.isHovered }">
       <v-container fluid>
         <RouterView />
       </v-container>
@@ -10,16 +21,9 @@
     <Footer />
   </v-app>
 </template>
-<script setup>
-
-import Header from "@/components/common/Header.vue";
-import Footer from "@/components/common/Footer.vue";
-import Sidebar from "@/components/common/Sidebar.vue";
-import { ref } from "vue";
-const drawer = ref(true);
-</script>
 
 <style>
-
-@import '@/assets/main.css';
+.with-sidebar-expanded {
+  margin-left: 256px; /* Adjust based on expanded width of drawer */
+}
 </style>
--- Front-End/src/components/common/Sidebar.vue
+++ Front-End/src/components/common/Sidebar.vue
@@ -1,20 +1,27 @@
 <template>
-    <v-navigation-drawer app v-model="drawer" permanent>
-      <v-list>
-        <v-list-item link to="/">
-          <v-list-item-title>Home</v-list-item-title>
-        </v-list-item>
-        <v-list-item link to="/about">
-          <v-list-item-title>About</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </template>
-  
-  <script setup>
-  import { ref, defineExpose } from 'vue'
-  
-  const drawer = ref(true)
-  defineExpose({ drawer })
-  </script>
-  
+  <v-navigation-drawer
+    app
+    v-model="drawer"
+    expand-on-hover
+    rail
+    @mouseenter="isHovered = true"
+    @mouseleave="isHovered = false"
+  >
+    <v-list>
+      <v-list-item link to="/">
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item>
+      <v-list-item link to="/about">
+        <v-list-item-title>About</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+import { ref, defineExpose } from 'vue'
+
+const drawer = ref(true)
+const isHovered = ref(false)
+defineExpose({ drawer, isHovered })
+</script>
